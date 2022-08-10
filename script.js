69c96dea1ab81a81ce4d5eaa86300e45fc3ee7cd
caclm10 <lewinxander@gmail.com>
2022-08-10 13:03:53 +0700

    add generate quote functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function showNotif(message, type) {
+    const notif = {
+        container: document.getElementById(`notif-container`),
+        content: document.getElementById(`notif-content`)
+    }
+
+    notif.container.classList.add(`notif-container-${type}`)
+    notif.container.classList.add('show')
+    notif.content.textContent = message
+}
+
+function listenCloseNotif() {
+    const btn = document.getElementById(`notif-close-button`)
+    const notifContainer = document.getElementById(`notif-container`)
+
+    btn.addEventListener('click', () => {
+        notifContainer.classList.remove('show')
+    })
+}
+
 function runButton() {
     const buttons = document.querySelectorAll(`button`)
 
@@ -29,7 +49,41 @@ function runButton() {
     }
 }
 
+async function fetchQuote() {
+    const url = `https://api.quotable.io/random`
+
+    const resp = await fetch(url)
+
+    if (!resp.ok) throw resp
+
+    const { author, content } = await resp.json()
+
+    return { author, content }
+}
+
+async function generateQuote() {
+    const loaderContainer = document.getElementById(`loader-container`)
+
+    loaderContainer.classList.add('show')
+    try {
+        const { author, content } = await fetchQuote()
+
+        document.getElementById(`author-name`).textContent = author
+        document.getElementById(`quote-content`).textContent = content
+    } catch (error) {
+        showNotif(error.message, 'error')
+    }
+    loaderContainer.classList.remove('show')
+}
 
 window.addEventListener('DOMContentLoaded', () => {
+    const newQuoteButton = document.getElementById(`new-quote-button`)
+
     runButton()
+
+    listenCloseNotif()
+
+    generateQuote()
+
+    newQuoteButton.addEventListener('click', generateQuote)
 })
